dd125a400feb9d99c4d514708bb0e2f52a8dc96e
Pierre Gunter <gunter.pierre@gmail.com>
2021-08-17 19:22:34 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rovio_match_making
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
