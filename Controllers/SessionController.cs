136da21116568532f466c35eb987b59a45cf3728
Pierre Gunter <gunter.pierre@gmail.com>
2021-08-23 18:42:29 +0200

    Add the API skeleton

---
+++ Controllers/SessionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Rovio.MatchMaking.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SessionController : ControllerBase
+    {
+        private readonly ILogger<SessionController> _logger;
+
+        public SessionController(ILogger<SessionController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+            // var rng = new Random();
+            // return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            // {
+            //     Date = DateTime.Now.AddDays(index),
+            //     TemperatureC = rng.Next(-20, 55),
+            //     Summary = Summaries[rng.Next(Summaries.Length)]
+            // })
+            // .ToArray();
+        }
+    }
+}
