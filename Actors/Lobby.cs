53846ddc70e3e7319ce4d1fa63ceebe3bbeaf00a
Pierre Gunter <gunter.pierre@gmail.com>
2021-08-25 21:06:37 +0200

    Add the logic to create a session

--- Actors/Lobby.cs
+++ Actors/Lobby.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Akka.Actor;
 
 namespace Rovio.MatchMaking.Actors
@@ -8,7 +9,7 @@ namespace Rovio.MatchMaking.Actors
     {
         public static Props Props() => Akka.Actor.Props.Create(() => new Lobby());
 
-        private Dictionary<Guid, Ticket> _tickets = new();
+        private Dictionary<Guid, Ticket> _ticketLookup = new();
 
         public Lobby()
         {
@@ -21,28 +22,105 @@ namespace Rovio.MatchMaking.Actors
 
         private void Handle(CreateSession command)
         {
-            if (_tickets.Count < command.MinPlayerCount)
+            // Not enough tickets to create a session
+            if (_ticketLookup.Count < command.MinPlayerCount)
             {
-                Context.Sender.Tell("test value", Self);
+                Context.Sender.Tell(new Session(command.LobbyId), Self);
             }
+            // Not enough tickets to calculate mean, but enough to create a session
+            else if (_ticketLookup.Count < 2)
+            {
+                Context.Sender.Tell(new Session(command.LobbyId, _ticketLookup.Values.ToList()), Self);
+                _ticketLookup.Clear();
+            }
+
+            // Calculate the mean using Welford's online algorithm
+            double mean = 0d, m2 = 0d, delta1 = 0d, delta2 = 0d;
+            int n = 0;
+            foreach (var ticket in _ticketLookup.Values)
+            {
+                n++;
+                delta1 = ticket.Latency - mean;
+                mean += delta1 / n;
+                delta2 = ticket.Latency - mean;
+                m2 += delta1 * delta2;
+            }
+
+            double variance = m2 / n;
+
+            var now = NodaTime.SystemClock.Instance.GetCurrentInstant().ToUnixTimeTicks();
+            double timeFactor = 1d / command.MaxWaitTime;
+
+            var tickets = _ticketLookup.Values.ToList();
+            tickets.Sort((x, y) =>
+            {
+                var result = GetAdjustedDeviation(x.Latency, mean, now - x.RegisteredAt, command.MaxWaitTime, timeFactor).CompareTo(
+                    GetAdjustedDeviation(y.Latency, mean, now - y.RegisteredAt, command.MaxWaitTime, timeFactor));
+
+                return result == 0
+                    ? x.Latency.CompareTo(y.Latency)
+                    : result;
+            });
+
+            var session = new Session(command.LobbyId, tickets.GetRange(0, Math.Min(command.MaxPlayerCount, tickets.Count)));
+            foreach (var ticket in session.Tickets)
+            {
+                _ticketLookup.Remove(ticket.Id);
+            }
+
+            Sender.Tell(session, Self);
         }
 
         private void Handle(CancelTicket command)
         {
-            _tickets.Remove(command.TicketId);
+            _ticketLookup.Remove(command.TicketId);
         }
 
         private void Handle(Ticket command)
         {
-            if (!_tickets.TryAdd(command.Id, command))
+            if (!_ticketLookup.TryAdd(command.Id, command))
             {
-                _tickets[command.Id] = command;
+                _ticketLookup[command.Id] = command;
             }
         }
 
         #endregion Handlers
 
-        #region Commands
+        private double GetAdjustedDeviation(double latency, double mean, long waitTime, long maxWaitTime, double timeFactor)
+        {
+            if (maxWaitTime <= waitTime)
+            {
+                return 0;
+            }
+            else if (waitTime <= 0)
+            {
+                return Math.Abs(mean - latency);
+            }
+            else
+            {
+                return Math.Abs(mean - latency) * timeFactor * (maxWaitTime - waitTime);
+            }
+        }
+
+        #region Messages
+        public class Session
+        {
+            public Guid LobbyId { get; }
+            public List<Ticket> Tickets { get; }
+
+            public Session(Guid lobbyId)
+            {
+                Tickets = new();
+                LobbyId = lobbyId;
+            }
+
+            public Session(Guid lobbyId, List<Ticket> tickets)
+            {
+                LobbyId = lobbyId;
+                Tickets = tickets;
+            }
+        }
+
         public class Ticket
         {
             public Guid GameId { get; }
@@ -73,17 +151,19 @@ namespace Rovio.MatchMaking.Actors
 
         public class CreateSession
         {
-            public CreateSession(Guid gameId, uint minPlayerCount, uint maxPlayerCount)
+            public CreateSession(Guid lobbyId, int minPlayerCount, int maxPlayerCount, long maxWaitTime)
             {
-                GameId = gameId;
+                LobbyId = lobbyId;
                 MinPlayerCount = minPlayerCount;
                 MaxPlayerCount = maxPlayerCount;
+                MaxWaitTime = maxWaitTime;
             }
 
-            public Guid GameId { get; }
-            public uint MinPlayerCount { get; }
-            public uint MaxPlayerCount { get; }
+            public Guid LobbyId { get; }
+            public int MinPlayerCount { get; }
+            public int MaxPlayerCount { get; }
+            public long MaxWaitTime { get; }
         }
-        #endregion Commands
+        #endregion Messages
     }
 }
--- Controllers/SessionsController.cs
+++ Controllers/SessionsController.cs
@@ -24,7 +24,7 @@ namespace Rovio.MatchMaking.Controllers
         [HttpPost]
         public async Task<IActionResult> PostAsync([FromBody]Guid game)
         {
-            var result = await _deliveryActor.Ask(new Actors.Lobby.CreateSession(game, 5, 5), TimeSpan.FromSeconds(60));
+            var result = await _deliveryActor.Ask(new Actors.Lobby.CreateSession(game, 5, 5, NodaTime.Duration.FromSeconds(360).BclCompatibleTicks), TimeSpan.FromSeconds(60));
 
             return Ok(new
             {
