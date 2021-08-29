60a8e3f47b74c1a73b816b3c0a2129c6a601fe60
Pierre Gunter <gunter.pierre@gmail.com>
2021-08-29 22:09:01 +0200

    Add testing packages

---
+++ Rovio.MatchMaking.Tests/LobbyTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Rovio.MatchMaking.Tests
+{
+    public class LobbyTests : Akka.TestKit.
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
